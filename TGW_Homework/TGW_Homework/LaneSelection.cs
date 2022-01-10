857c6300f745dae30bef40e7819dad5979aa4e32
Tomras2 <tomorune@gmail.com>
2022-01-10 02:28:19 +0200

    updated and added comments

--- TGW_Homework/TGW_Homework/LaneSelection.cs
+++ TGW_Homework/TGW_Homework/LaneSelection.cs
@@ -12,7 +12,12 @@ namespace TGW_Homework
         {
 
         }
-
+        /// <summary>
+        /// Method to make destination array values 0 at first
+        /// </summary>
+        /// <param name="arr">destination values array</param>
+        /// <param name="n">array length</param>
+        /// <returns>destination values array with 0 as initial values</returns>
         public int[] FillEmptyArray(int[] arr, int n)
         {
             for (int i = 0; i < n; i++)
@@ -21,9 +26,18 @@ namespace TGW_Homework
             }
             return arr;
         }
-
+        /// <summary>
+        /// Round robin algorithm which takes destionations, conLoads, failurePercent and loads and uses the classic RoundRobin
+        /// method to keep dividing loads for each destination one after another
+        /// </summary>
+        /// <param name="destinations">destination number</param>
+        /// <param name="conLoads">consecutive load number</param>
+        /// <param name="failurePercent">percentage of failure for divert loads</param>
+        /// <param name="loads">total loads</param>
         public void RoundRobinAlgorithm(int destinations, int conLoads, int failurePercent, int loads)
         {
+            //Destination values Array to keep track of the values, also calculations of neededIterations and
+            // failureLoads count, aswell as whether there is a lastLoad which is less than given conLoad number
             int[] destinationArray = new int[destinations + 1];
             FillEmptyArray(destinationArray, destinations);
             int neededIterations = loads / conLoads;
@@ -35,6 +49,7 @@ namespace TGW_Homework
                 neededIterations++;
                 lastLoad = loads % conLoads;
             }
+            //Firstly check according to the destinations whether they must start from 1 again or go till last destination
             for (int i = 0; i < neededIterations; i++)
             {
                 if (destinationNumber < destinations) {
@@ -44,6 +59,9 @@ namespace TGW_Homework
                 {
                     destinationNumber = 1;
                 }
+
+                //Then check and update values of destinations according to conLoads and failurePercent
+                //And also check whether lastLoad exist which is the last iteration of a not full consecutive load
 
                 if (lastLoad == 0 || (lastLoad > 0 && i != neededIterations - 1))
                 {
@@ -60,15 +78,17 @@ namespace TGW_Homework
                 }
              
             }
-
-            for (int i = 0;i < destinationArray.Length; i++)
-            {
-                Console.WriteLine("Destination " + i);
-                Console.WriteLine(destinationArray[i]);
-            }
+            PrintPercentages(destinationArray, loads);
 
         }
-
+        /// <summary>
+        /// Random algorithm which takes destionations, conLoads, failurePercent and loads and divides loads using a random
+        /// number generator with same percent for each destination
+        /// </summary>
+        /// <param name="destinations">destination number</param>
+        /// <param name="conLoads">consecutive load number</param>
+        /// <param name="failurePercent">percentage of failure for divert loads</param>
+        /// <param name="loads">total loads</param>
         public void RandomAlgorithm(int destinations, int conLoads, int failurePercent, int loads)
         {
             int[] destinationArray = new int[destinations + 1];
@@ -104,10 +124,21 @@ namespace TGW_Homework
 
             }
 
+            PrintPercentages(destinationArray, loads);
+
+        }
+        /// <summary>
+        /// Print method to print destinations and their percentages
+        /// </summary>
+        /// <param name="destinationArray">Destination values array</param>
+        /// <param name="loads">Total loads</param>
+        public void PrintPercentages(int[] destinationArray, int loads)
+        {
             for (int i = 0; i < destinationArray.Length; i++)
             {
                 Console.WriteLine("Destination " + i);
-                Console.WriteLine(destinationArray[i]);
+                double percentage = Convert.ToDouble(destinationArray[i]) / loads * 100;
+                Console.WriteLine(percentage +"%");
             }
 
         }
